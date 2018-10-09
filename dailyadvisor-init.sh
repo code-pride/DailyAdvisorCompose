b0eed5175a0702ffcdf3263a8600310ef1bae5de
benq95 <woj.bedkowski@gmail.com>
2018-10-09 18:47:40 +0200

    Added pull in case of exisint directories.

--- dailyadvisor-init.sh
+++ dailyadvisor-init.sh
@@ -11,7 +11,8 @@ cd $DIR
 
 if [ -e $DIR/DailyAdvisor-PWA ] ;then
     echo "DailyAdvisor-PWA project already exists"
-    echo "Skipping"
+    echo "Updating"
+    git -C $DIR/DailyAdvisor-PWA pull
 else
     git clone https://github.com/code-pride/DailyAdvisor-PWA.git $DIR/DailyAdvisor-PWA
     echo "Project DailyAdvisor-PWA has been cloned"
@@ -19,7 +20,8 @@ fi
 
 if [ -e $DIR/api-service ] ;then
     echo "api-service project already exists"
-    echo "Skipping"
+    echo "Updating"
+    git -C $DIR/api-service pull
 else
     git clone -b task/no-login https://github.com/code-pride/DailyAdvisor-API.git $DIR/api-service
     echo "Project api-service has been cloned"
@@ -27,7 +29,8 @@ fi
 
 if [ -e $DIR/user-service ] ;then
     echo "user-service project already exists"
-    echo "Skipping"
+    echo "Updating"
+    git -C $DIR/user-service pull
 else
     git clone -b task/docker https://github.com/code-pride/UserService.git $DIR/user-service
     echo "Project user-service has been cloned"
@@ -35,7 +38,8 @@ fi
 
 if [ -e $DIR/eureka ] ;then
     echo "Eureka project already exists"
-    echo "Skipping"
+    echo "Updating"
+    git -C $DIR/eureka pull
 else
     git clone -b develop https://github.com/code-pride/EurekaDiscovery.git $DIR/eureka
     echo "Project eureka has been cloned"
@@ -43,15 +47,18 @@ fi
 
 if [ -e $DIR/gateway ] ;then
     echo "Gateway project already exists"
-    echo "Skipping"
+    echo "Updating"
+    git -C $DIR/DailyAdvisor-PWA pull
 else
     git clone -b spring-gateway https://github.com/code-pride/Gateway.git $DIR/gateway
     echo "Project gateway has been cloned"
+    git -C $DIR/gateway pull
 fi
 
-if [ -e $DIR/Tests ] ;then
+if [ -e $DIR/tests ] ;then
     echo "Tests project already exists"
-    echo "Skipping"
+    echo "Updating"
+    git -C $DIR/tests pull
 else
     git clone -b develop https://github.com/code-pride/IntegrationTests.git $DIR/tests
     echo "Project tests has been cloned"
